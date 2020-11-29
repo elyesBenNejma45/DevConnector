4400f9bf690e43fa415f20ca2417b474a98a8e7b
elyesBenNejma45 <elyes.bennejma45@gmail.com>
2020-11-29 21:03:31 +0100

    create multiple routes and user schema

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURl");
+
+const connectDB = async() =>{
+    try {
+        mongoose.connect(db,{
+            useUnifiedTopology: true ,
+            useNewUrlParser: true
+        });
+        console.log("database connected...")       
+    } catch (error) {
+        console.error(error.message);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-app.listen(port,()=> console.log(`server started on port ${port}`));
+const connectDB = require("./config/db");
 
+connectDB();
+app.use("/api/users",require("./routes/api/users.js"))
+app.use("/api/auth",require("./routes/api/auth.js"))
+app.use("/api/posts",require("./routes/api/posts.js"))
+app.use("/api/profile",require("./routes/api/profile.js"))
+
+app.listen(port,()=> console.log(`server started on port ${port}`));
 app.get('/',(req,res)=>res.send('API running'));
