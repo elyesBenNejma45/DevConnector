a76f371c7e18b7a5b364e7389469339d4c531af8
elyesBenNejma45 <elyes.bennejma45@gmail.com>
2020-12-09 18:39:06 +0100

    add client part and install react

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+ const Navbar = () => {
+    return (
+        <nav className="navbar bg-dark">
+        <h1>
+            <Link to ="/"><i className="fas fa-code"></i> DevConnector</Link>
+        </h1>
+        <ul>
+            <li>
+                <Link to="!#">Developers</Link>
+            </li>
+            <li>
+                <a href="/register">Register</a>
+            </li>
+            <li>
+                <a href="/login">Login</a>
+            </li>
+        </ul>
+        </nav>
+    )
+}
+
+export default Navbar
